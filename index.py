b7e8bd38966f976a5acd0ad5e918c82801eaf3aa
Andrei Savu <savu.andrei@gmail.com>
2009-05-01 11:38:58 +0300

    basic templates layout in place

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
 
-import web
+import web, settings
 
 urls = (
     '^/$', 'index'
 )
 
 app = web.application(urls, globals())
+render = web.template.render(settings.TEMPLATE_FOLDER, base='base')
 
 class index:
 	def GET(self):
-		return "Hello world"
+		return render.index()
 
 if __name__ == "__main__":
     app.run()
