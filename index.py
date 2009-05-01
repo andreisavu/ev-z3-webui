08103b5316f9a15d1729759a0e047edae9c205bf
Andrei Savu <savu.andrei@gmail.com>
2009-05-01 12:20:13 +0300

    template placeholders & basic controllers

--- index.py
+++ index.py
@@ -2,7 +2,11 @@
 import web, settings
 
 urls = (
-    '^/$', 'index'
+    '^/$', 'index',
+	'^/exclusive$', 'exclusive',
+	'^/edit$', 'edit',
+	'^/upload$', 'upload',
+	'^/about$', 'about'
 )
 
 app = web.application(urls, globals())
@@ -11,6 +15,24 @@ render = web.template.render(settings.TEMPLATE_FOLDER, base='base')
 class index:
 	def GET(self):
 		return render.index()
+
+class exclusive:
+	def POST(self):
+		raise web.seeother('/edit')
+
+class edit:
+	def GET(self):
+		return render.edit()
+
+class upload:
+	def  GET(self):
+		return render.upload()
+	def POST(self):
+		raise web.seeother('/edit')
+
+class about:
+	def GET(self):
+		return render.about()
 
 if __name__ == "__main__":
     app.run()
