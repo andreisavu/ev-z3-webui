89c5dfd436fbe16a80e07ad000176ff28d47e9c6
root <root@mobdev.(none)>
2009-05-03 20:11:13 +0300

    placeholders for serial send tools

---
+++ tools/receiver.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# Simple serial receiver
+
+__author__ = 'Savu Andrei <contact@andreisavu.ro>'
+
+import serial
+
+if __name__ == '__main__':
+	print 'Seria receiver'
+
---
+++ tools/sender.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+# Simple serial binary image sender
+
+__author__ = 'Savu Andrei <contact@andreisavu.ro>'
+
+import serial
+
+if __name__ == '__main__':
+	
+	print 'Serial sender'
+
