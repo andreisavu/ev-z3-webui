3747e991f357f85444170346107db10cf73e44d1
Andrei Savu <savu.andrei@gmail.com>
2009-05-04 00:37:50 +0300

    simple sender with hardware flow control

--- tools/sender.py
+++ tools/sender.py
@@ -5,8 +5,37 @@
 __author__ = 'Savu Andrei <contact@andreisavu.ro>'
 
 import serial
+import sys, os
 
-if __name__ == '__main__':
+def parse_cli_params():
+	if len(sys.argv) != 3:
+		print 'Usage: ./sender.py <port_no> <file>'
+		sys.exit(0)
+	if not os.path.exists(sys.argv[2]):
+		print 'File not found. Check parameters'
+		sys.exit(0)
+	return int(sys.argv[1]), sys.argv[2]
+
+def open_port(port):
+	try:
+		s = serial.Serial(port=port, baudrate=9600, rtscts=1)
+		return s
+	except:
+		return None
 	
-	print 'Serial sender'
+if __name__ == '__main__':
+	port, file = parse_cli_params()
+	s = open_port(port)
+	if s == None:
+		print 'Unable to open port'
+		sys.exit(0)
+		
+	if not s.isOpen():
+		s.open()
+	
+	if s.isOpen():
+		print 'Writing ...'
+		s.write(open(file).read())
+		
+		
 
